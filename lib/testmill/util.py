c02b7f2b9128bafd0a22dd9669e32bc99170f75d
Geert Jansen <geertj@gmail.com>
2013-01-05 02:18:34 +0100

    Extend copyright claim to 2013

--- lib/testmill/util.py
+++ lib/testmill/util.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Ravello Systems, Inc.
+# Copyright 2012-2013 Ravello Systems, Inc.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
