ef22b5cea1cd064db065775fea9e878fc3be7715
Geert Jansen <geertj@gmail.com>
2013-02-18 14:54:25 +0200

    Now working on version 0.9.9.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 
 version_info = {
     'name': 'testmill',
-    'version': '0.9.8',
+    'version': '0.9.9.dev',
     'description': 'Create multi-VM application environments for dev/test.',
     'author': 'Geert Jansen',
     'author_email': 'geert.jansen@ravellosystems.com',
